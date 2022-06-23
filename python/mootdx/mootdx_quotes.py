083d12af2f4d64f7a64682eef16e17271a77d958
gaudi.gao <clonalman@hotmail.com>
2022-06-23 16:18:50 +0800

    mootdt redis ts

--- python/mootdx/mootdx_quotes.py
+++ python/mootdx/mootdx_quotes.py
@@ -6,51 +6,12 @@ import traceback
 from datetime import datetime
 
 from pytdx.parser.base import ResponseHeaderRecvFails, ResponseRecvFails
-
 from mootdx_rts import MootdxRTS, make_pid
-
-
-def run(std, symbols):
-    # 实时分时行情
-    num = 0
-    print("Batch symbols: " + str(len(symbols)))
-
-    while num < len(symbols):
-        start_dt = datetime.now()
-        try:
-            items = symbols[num:num + 80]
-            print(items)
-            num = num + 80
-            qdf = std.quotes(items)
-            if qdf is not None:
-                std.save(datetime.now().date(), qdf)
-            end_dt = datetime.now()
-            print('cost: %dms' % ((end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
-        except (ConnectionAbortedError,
-                ConnectionRefusedError,
-                ConnectionResetError,
-                ResponseRecvFails,
-                ResponseHeaderRecvFails):
-            traceback.print_exc()
-            end_dt = datetime.now()
-            print('cost: %dms(error)' % ((end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
-            std = MootdxRTS.std()
-            time.sleep(1)
-        except Exception:
-            traceback.print_exc()
-            end_dt = datetime.now()
-            print('cost: %dms(exception)' % (
-                        (end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
-            time.sleep(1)
-
-    r_t01 = threading.Timer(0.2, run, args=(std, symbols,))
-    r_t01.start()
 
 
 def get_stock_symbols():
     result = []
-    # '600', '601', '603', '688'
-    # '000', '002', '300'
+    # ["600", "601", "603", "688", "000", "002", "300"]
     with open(sys.path[0] + '/market/stocks.json', encoding='utf-8') as f1:
         json_stocks = json.load(f1)
         with open(sys.path[0] + '/redis-symbol.json', encoding='utf-8') as f2:
@@ -58,26 +19,64 @@ def get_stock_symbols():
             for stock in json_stocks:
                 if stock['code'].startswith(tuple(json_symbols)):
                     result.append(stock['code'])
-    print("Total symbols: " + str(len(result)))
-    print("====================================")
-    print(result)
+    print("Total symbols:", len(result), 'Now:', time.strftime('%H:%M:%S', time.localtime()))
     print("====================================")
     return result
+
+
+def createTimer(std, symbols):
+    t = threading.Timer(0.2, repeat, args=(std, symbols,))
+    t.start()
+
+
+def repeat(std, symbols):
+    print('Now:', time.strftime('%H:%M:%S', time.localtime()), "Symbols:", len(symbols))
+    print("------------------------------------")
+    # 实时分时行情
+    num = 0
+    while num < len(symbols):
+        start_dt = datetime.now()
+        try:
+            items = symbols[num:num + 80]
+            print(items)
+            qdf = std.quotes(items)
+            if qdf is not None:
+                std.save(datetime.now().date(), qdf)
+
+            end_dt = datetime.now()
+            num = num + 80
+            print('Now:', time.strftime('%H:%M:%S', time.localtime()),
+                  'cost:', '%dms' % ((end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
+        except (ConnectionAbortedError,
+                ConnectionRefusedError,
+                ConnectionResetError,
+                ResponseRecvFails,
+                ResponseHeaderRecvFails):
+            traceback.print_exc()
+            end_dt = datetime.now()
+            print('Now:', time.strftime('%H:%M:%S', time.localtime()),
+                  'cost:', '%dms(error)' % ((end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
+            std = MootdxRTS.std()
+            time.sleep(1)
+        except Exception:
+            traceback.print_exc()
+            end_dt = datetime.now()
+            print('Now:', time.strftime('%H:%M:%S', time.localtime()),
+                  'cost:', '%dms(exception)' % ((end_dt - start_dt).seconds * 1000 + (end_dt - start_dt).microseconds / 1000))
+            time.sleep(1)
+
+    createTimer(std, symbols)
 
 
 if __name__ == '__main__':
     make_pid('mootdx_quotes')
 
-    thread_list = []
-    thread_size = 5
+    num_timer = 5
 
     stock_symbols = get_stock_symbols()
     total = len(stock_symbols)
     batch = 0
-    batch_size = int(total / thread_size) + 1
+    batch_size = int(total / num_timer) if total % num_timer == 0 else int(total / num_timer) + 1
     while batch < total:
-        std = MootdxRTS.std()
-        t01 = threading.Thread(target=run, args=(std, stock_symbols[batch:batch+batch_size]))
-        thread_list.append(t01)
-        t01.start()
+        createTimer(MootdxRTS.std(), stock_symbols[batch:batch + batch_size])
         batch += batch_size
